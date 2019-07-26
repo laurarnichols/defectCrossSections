c38f14d24e9c2c4008478e1e0e56d547b5394dba
laurarnichols <laurarnichols5@gmail.com>
2019-07-26 14:27:15 -0500

    Added documentation to main function

--- Sigma/src/Sigma_Main.f90
+++ Sigma/src/Sigma_Main.f90
@@ -1,15 +1,18 @@
 program crossSection
+  !! Combine the results of TME and LSF
+  !! into the cross section
   !
   use sigma_module
   !
   implicit none
   !
-  ! Reading input, initializing and checking all variables of the calculation.
-  !
   call readInputs()
+    !! * Read input, initializing and checking all variables of the calculation
   !
   call calculateSigma()
+    !! * Calculate \(\sigma(E)\)
   !
   call writeSigma()
+    !! * Output \(\sigma(E)\)
   !
 end program crossSection
