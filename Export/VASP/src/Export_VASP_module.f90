46a9d1c2304ceb1f52aa77c7972abfb8ab020195
Laura Nichols <laurarnichols5@gmail.com>
2020-06-29 15:01:26 -0400

    Added QE code exactly here

--- Export/VASP/src/Export_VASP_module.f90
+++ Export/VASP/src/Export_VASP_module.f90
@@ -1227,6 +1227,36 @@ module wfcExportVASPMod
   subroutine getNumGkVectors()
     implicit none
 
+    npwx = 0
+    do nk = 1, nks
+      ngk (nk) = 0
+      do ng = 1, ngm
+        q2 = (xk (1, nk) + g (1, ng) ) **2 + (xk (2, nk) + g (2, ng) ) ** &
+             2 + (xk (3, nk) + g (3, ng) ) **2
+        if (q2 <= ecutwfc / tpiba2) then
+          !
+          ! here if |k+G|^2 <= Ecut increase the number of G inside the sphere
+          !
+          ngk (nk) = ngk (nk) + 1
+        else
+          if (sqrt (g (1, ng) **2 + g (2, ng) **2 + g (3, ng) **2) &
+               .gt.sqrt (xk (1, nk) **2 + xk (2, nk) **2 + xk (3, nk) **2) &
+               + sqrt (ecutwfc / tpiba2) ) goto 100
+          !
+          ! if |G| > |k| + sqrt(Ecut)  stop search
+          !
+        endif
+      enddo
+100   npwx = max (npwx, ngk (nk) )
+    enddo
+
+    if (npwx <= 0) call errore ('n_plane_waves', &
+                'No plane waves found: running on too many processors?', 1)
+
+    ! when using pools, set npwx to the maximum value across pools
+    ! (you may run into trouble at restart otherwise)
+
+    CALL mp_max ( npwx, inter_pool_comm )
 
     return
   end subroutine getNumGkVectors
