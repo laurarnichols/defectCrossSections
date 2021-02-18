82b4978c0a23b280c2f924ded4fbe10d7c247cbc
Laura Nichols <laurarnichols5@gmail.com>
2021-02-18 10:38:25 -0600

    Moved int2str here so don't have to worry about module paths

--- Export/VASP_dependent/src/ExportFromVASP.f90
+++ Export/VASP_dependent/src/ExportFromVASP.f90
@@ -72,7 +72,6 @@
 !**********************************************************************
 
 program VASPExport
-      use miscUtilities
 
 
       USE prec
@@ -2673,5 +2672,31 @@ program VASPExport
       CALL DUMP_FINAL_TIMING(IO%IU6)
       CALL STOP_XML
       CALLMPI_C(M_exit())
+
+  contains
+
+  !---------------------------------------------------------------------------------------------------------------------------------
+  subroutine int2str(integ, string)
+    !! Write a given integer to a string, using only as many digits as needed
+    !
+    implicit none
+    integer :: integ
+    character(len = 300) :: string
+    !
+    if ( integ < 10 ) then
+      write(string, '(i1)') integ
+    else if ( integ < 100 ) then
+      write(string, '(i2)') integ
+    else if ( integ < 1000 ) then
+      write(string, '(i3)') integ
+    else if ( integ < 10000 ) then
+      write(string, '(i4)') integ
+    endif
+    !
+    string = trim(string)
+    !
+    return
+    !
+  end subroutine int2str
 
 end program VASPExport
