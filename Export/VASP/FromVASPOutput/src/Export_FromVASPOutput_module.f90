8a4ae1431a14403645c9c26deeb7412a157b6b74
Laura Nichols <laurarnichols5@gmail.com>
2022-11-15 11:19:18 -0600

    Add psedopotential subroutine with code as-is

--- Export/VASP/FromVASPOutput/src/Export_FromVASPOutput_module.f90
+++ Export/VASP/FromVASPOutput/src/Export_FromVASPOutput_module.f90
@@ -2612,6 +2612,8 @@ module wfcExportVASPMod
 
       do ip = 1, ps(iT)%nChannels
 
+        call getPseudopotential()
+
         angMom = ps(iT)%angMom(ip)
         imMax= 2*angMom
         LMBASE = angMom**2 + 1
@@ -2664,6 +2666,48 @@ module wfcExportVASPMod
 
     return
   end subroutine calculateRealProjWoPhase
+
+!----------------------------------------------------------------------------
+  subroutine getPseudopotential()
+    implicit none
+
+     FAKT= 1/SQRT(LATT_CUR%OMEGA)
+     ARGSC=NPSNL/P(NT)%PSMAXN
+     DO IND=1,INDMAX
+        ARG=(GLEN(IND)*ARGSC)+1
+        NADDR=INT(ARG)
+
+        VPS(IND)=0._q
+
+        IF (ASSOCIATED(P(NT)%PSPNL_SPLINE)) THEN
+           IF (NADDR<NPSNL) THEN
+              NADDR  =MIN(INT(GLEN(IND)*ARGSC)+1,NPSNL-1)
+              REM=GLEN(IND)-P(NT)%PSPNL_SPLINE(NADDR,1,L)
+              VPS(IND)=(P(NT)%PSPNL_SPLINE(NADDR,2,L)+REM*(P(NT)%PSPNL_SPLINE(NADDR,3,L)+ &
+                &         REM*(P(NT)%PSPNL_SPLINE(NADDR,4,L)+REM*P(NT)%PSPNL_SPLINE(NADDR,5,L))))*FAKT*FAKTX(IND)
+           ENDIF
+        ELSE IF (NADDR<NPSNL-2) THEN
+           REM=MOD(ARG,1.0_q)
+           V1=P(NT)%PSPNL(NADDR-1,L)
+           V2=P(NT)%PSPNL(NADDR,L  )
+           V3=P(NT)%PSPNL(NADDR+1,L)
+           V4=P(NT)%PSPNL(NADDR+2,L)
+           T0=V2
+           T1=((6*V3)-(2*V1)-(3*V2)-V4)/6._q
+           T2=(V1+V3-(2*V2))/2._q
+           T3=(V4-V1+(3*(V2-V3)))/6._q
+           VPS(IND)=(T0+REM*(T1+REM*(T2+REM*T3)))*FAKT*FAKTX(IND)
+        ENDIF
+
+        IF (VPS(IND)/=0._q.AND.PRESENT(DK)) THEN
+           ARG=(GLENP(IND)*ARGSC)+1
+           NADDR=INT(ARG)
+           IF (NADDR>NPSNL-1.OR.(.NOT.ASSOCIATED(P(NT)%PSPNL_SPLINE).AND.NADDR>NPSNL-3)) VPS(IND)=0._q
+        ENDIF
+     ENDDO
+
+    return
+  end subroutine getPseudopotential
 
 !----------------------------------------------------------------------------
   subroutine writeKInfo(nKPoints, maxNumPWsPool, gKIndexLocalToGlobal, nBands, nGkLessECutGlobal, nGkLessECutLocal, &
