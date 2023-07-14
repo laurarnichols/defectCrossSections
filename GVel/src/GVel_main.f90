262fde77813ec7a8bc1e6b7a6182526d872829f8
Laura Nichols <laurarnichols5@gmail.com>
2023-07-14 14:10:50 -0500

    Add reading eigs and spin polarization

--- GVel/src/GVel_main.f90
+++ GVel/src/GVel_main.f90
@@ -4,7 +4,7 @@ program GVel
 
   implicit none
 
-  integer :: ikLocal, ikGlobal, ib, ix, ibd, iDegen
+  integer :: ikLocal, ikGlobal, ib, ix, ibd, iDegen, isp
     !! Loop indices
 
   real(kind=dp) :: t0, t1, t2
@@ -24,74 +24,80 @@ program GVel
 
 
   ! Get inputs:
-  !  * nKPoints (get only number of middle k-points)
+  !  * nBaseKPoints (get only number of middle k-points)
+  !  * nDispkPerCoord
+  !  * nSpins
   !  * iBandInit, iBandFinal
   !  * degenTol
+  !  * exportDir
 
-  call distributeItemsInSubgroups(myPoolId, nKPoints, nProcs, nProcPerPool, nPools, ikStart_pool, ikEnd_pool, nkPerPool)
+  call distributeItemsInSubgroups(myPoolId, nBaseKPoints, nProcs, nProcPerPool, nPools, ikStart_pool, ikEnd_pool, nkPerPool)
     !! * Distribute k-points in pools
 
 
-  allocate(eigv(iBandInit:iBandFinal,3,3))
+  call getBaseKPointIndex(nDispkPerCoord, iBaseK)
+
+
+  allocate(eigv(iBandInit:iBandFinal,nDispkPerCoord+1,3,nSpins))
+    ! Band, displacement (negative to positive), 
+    ! direction (x/y/z), and spin
   allocate(bandL(iBandInit:iBandFinal))
   allocate(bandR(iBandInit:iBandFinal))
 
-  do ikLocal = 1, nkPerPool
-    ! Iterate through middle k-points
+  do isp = 1, nSpins
+    do ikLocal = 1, nkPerPool
+      ! Iterate through middle k-points
 
-    ! call readEigenvalues(eigv)
-      ! Read eigenvalues for this k-point for all positions and directions
-      !
-      ! Assume that only one spin channel because group 
-      ! velocities come from perfect crystal.
+      call readGroupedEigenvalues(ikLocal, isp, nBands, nBaseKPoints, nDispkPerCoord, nSpins, eigv)
+        ! Read eigenvalues for this k-point for all positions and directions
 
-    do ix = 1, 3
+      do ix = 1, 3
 
-      ib = iBandInit
-      do while(ib <= iBandFinal)
+        ib = iBandInit
+        do while(ib <= iBandFinal)
 
-        ibd = ib + 1
+          ibd = ib + 1
 
-        ! Find any degenerate bands in this group
-        do while(ibd <= iBandFinal .and. abs(eigv(ibd,ix,2) - eigv(ib,ix,2) < degenTol))
-          ibd = ibd + 1
-        enddo
+          ! Find any degenerate bands in this group
+          do while(ibd <= iBandFinal .and. abs(eigv(ibd,ix,iBaseK,isp) - eigv(ib,ix,iBaseK,isp) < degenTol))
+            ibd = ibd + 1
+          enddo
 
-        nDegen = ibd - ib
+          nDegen = ibd - ib
 
-        if(nDegen == 1) then
-          ! If there are no degeneracies for this band,
-          ! assume that the band goes straight across
+          if(nDegen == 1) then
+            ! If there are no degeneracies for this band,
+            ! assume that the band goes straight across
 
-          bandL(ib) = ib
-          bandR(ib) = ib
+            bandL(ib) = ib
+            bandR(ib) = ib
 
   !       * Lock in those with good enough fits
           
-        else
-          ! If there are degeneracies, assume that bands
-          ! form a fan, where lowest bands on one side
-          ! correspond to highest bands on the other side
-          !
-          ! NOTE: I am not sure how bands should be assigned
-          ! here so that they properly line up with bands in
-          ! other directions. Still waiting on guidance from
-          ! Sok and Xiaoguang on how that should work.
+          else
+            ! If there are degeneracies, assume that bands
+            ! form a fan, where lowest bands on one side
+            ! correspond to highest bands on the other side
+            !
+            ! NOTE: I am not sure how bands should be assigned
+            ! here so that they properly line up with bands in
+            ! other directions. Still waiting on guidance from
+            ! Sok and Xiaoguang on how that should work.
 
-          ! Need to handle the case where last band is in 
-          ! degeneracy group
+            ! Need to handle the case where last band is in 
+            ! degeneracy group
 
         
-          do iDegen = 1, nDegen
-            bandL(ib+iDegen-1) = ib+iDegen-1
-              ! Left band goes lowest to highest
-            bandR(ib+iDegen-1) = ib+(nDegen-1)-(iDegen-1)
-              ! Right band goes highest to lowest
-          enddo
+            do iDegen = 1, nDegen
+              bandL(ib+iDegen-1) = ib+iDegen-1
+                ! Left band goes lowest to highest
+              bandR(ib+iDegen-1) = ib+(nDegen-1)-(iDegen-1)
+                ! Right band goes highest to lowest
+            enddo
 
-          ! NOTE: As far as I understand, the degeneracy groups
-          ! will be symmetric, and we don't care about the sign
-          ! of the slope, so the higher side is arbitrary.
+            ! NOTE: As far as I understand, the degeneracy groups
+            ! will be symmetric, and we don't care about the sign
+            ! of the slope, so the higher side is arbitrary.
             
 
   !         For each degeneracy:
@@ -99,12 +105,12 @@ program GVel
   !           * Lock in those with good enough fits
 
 
-          ib = ibd - 1
-            ! Make the outer band loop skip the degenerate bands 
-            ! that have already been handled
+            ib = ibd - 1
+              ! Make the outer band loop skip the degenerate bands 
+              ! that have already been handled
 
-        endif
-      enddo
+          endif
+        enddo
 
   !     * Output degenerate bands info
   !     * Output bands not locked in after first round as potential crossings
@@ -114,6 +120,7 @@ program GVel
   !         * If not locked in, check the fit of this point and the point above (not locked in) with the left or right points swapped 
   !         * Choose the one that makes both fits better
   !         * If the fits now meet the tolerance, lock them in
+      enddo
     enddo
   enddo
 
--- GVel/src/GVel_module.f90
+++ GVel/src/GVel_module.f90
@@ -1,7 +1,7 @@
 module GVelMod
   
   use constants, only: dp
-  use base, only: nKPoints
+  use base, only: nSpins
   use errorsAndMPI
 
   implicit none
@@ -10,13 +10,160 @@ module GVelMod
     !! Indices of left and right bands
   integer :: iBandInit, iBandFinal
     !! Initial and final bands
+  integer :: iBaseK
+    !! Index of base k-point within displacement pattern
+  integer :: nBands
+    !! Total number of bands
+  integer :: nBaseKPoints
+    !! Number of base k-points
   integer :: nDegen
     !! Number of degenerate bands
+  integer :: nDispkPerCoord
+    !! Number of displaced k-points per coordinate
 
   real(kind=dp) :: degenTol
     !! Tolerance for testing for degeneracies
-  real(kind=dp), allocatable :: eigv(:,:,:)
-    !! Eigenvalue for each band, direction
-    !! (x/y/z), and position (left/middle/right)
+  real(kind=dp), allocatable :: eigv(:,:,:,:)
+    !! Eigenvalue for each band, displacement 
+    !! (negative to positive), direction (x/y/z), 
+    !! and spin
 
+  character(len=300) :: exportDir
+    !! Directory to be used for export
+
+  contains
+
+!----------------------------------------------------------------------------
+  subroutine getBaseKPointIndex(nDispkPerCoord, iBaseK)
+    !! Get the index of the base k-point within
+    !! the displacement pattern in the `groupedEigenvalues.isp.ik`
+    !! files
+   
+    use miscUtilities, only: hpsort_eps
+
+    implicit none
+
+    ! Input variables:
+    integer, intent(in) :: nDispkPerCoord
+      !! Number of displaced k-points per coordinate
+
+    ! Output variables:
+    integer, intent(out) :: iBaseK
+      !! Index of base k-point within displacement pattern
+
+    ! Local variables:
+    integer :: idk
+      !! Loop index
+    integer :: iPattSort(nDispkPerCoord)
+      !! Sorted index order for patternArr from 
+      !! negative to positive
+
+    real(kind=dp) :: eps8 = 1.0E-8_dp
+      !! Double precision zero
+    real(kind=dp) :: patternArr(nDispkPerCoord+1)
+      !! Displacement pattern for groups of
+      !! k-points for group velocity calculations
+
+
+    if(ionode) then
+
+      patternArr(1) = 0.0_dp
+
+      open(72, file=trim(exportDir)//"/groupedEigenvaluesX.1.1")
+
+      read(72,*)
+      read(72,*)
+      read(72,*) (patternArr(idk), idk=2,nDispkPerCoord+1)
+
+      close(72)
+
+      do idk = 1, nDispkPerCoord
+        iPattSort(idk) = idk
+      enddo
+
+      call hpsort_eps(nDispkPerCoord+1, patternArr, iPattSort, eps8)
+        ! Get sorted index order for patternArr from negative to positive
+
+      do idk = 1, nDispkPerCoord+1
+        if(iPattSort(idk) == 1) then
+          iBaseK = idk
+          exit
+        endif
+      enddo
+
+    endif
+
+    call MPI_BCAST(iBaseK, 1, MPI_INTEGER, root, worldComm, ierr)
+
+    return
+
+  end subroutine
+
+!----------------------------------------------------------------------------
+  subroutine readGroupedEigenvalues(ikLocal, isp, nBands, nBaseKPoints, nDispkPerCoord, nSpins, eigv)
+
+    use miscUtilities, only: int2str, ignoreNextNLinesFromFile
+
+    implicit none
+
+    ! Input variables:
+    integer, intent(in) :: ikLocal
+      !! Local k-point index
+    integer, intent(in) :: isp
+      !! Spin index
+    integer, intent(in) :: nBands
+      !! Total number of bands
+    integer, intent(in) :: nBaseKPoints
+      !! Total number of k-points
+    integer, intent(in) :: nDispkPerCoord
+      !! Number of displaced k-points per coordinate
+    integer, intent(in) :: nSpins
+      !! Number of spins
+
+    ! Output variables:
+    real(kind=dp), intent(out) :: eigv(iBandInit:iBandFinal,nDispkPerCoord+1,3,nSpins)
+      !! Eigenvalue for each band, displacement 
+      !! (negative to positive), direction (x/y/z), 
+      !! and spin
+
+
+    ! Local variables
+    integer :: ikGlobal
+      !! Global k-point index
+    integer :: ib, idk, ix
+      !! Loop indices
+
+
+    if(indexInPool == 0) then
+
+      ikGlobal = ikLocal+ikStart_pool-1
+
+      do ix = 1, 3
+
+        if(ix == 1) then
+          open(72, file=trim(exportDir)//"/groupedEigenvaluesX."//trim(int2str(isp))//"."//trim(int2str(ikGlobal)))
+        else if(ix == 2) then
+          open(72, file=trim(exportDir)//"/groupedEigenvaluesY."//trim(int2str(isp))//"."//trim(int2str(ikGlobal)))
+        else if(ix == 3) then
+          open(72, file=trim(exportDir)//"/groupedEigenvaluesZ."//trim(int2str(isp))//"."//trim(int2str(ikGlobal)))
+        endif
+        
+        call ignoreNextNLinesFromFile(72,4)
+      
+        do ib = 1, nBands
+
+          read(72,*) (eigv(ib,idk,ix,isp), idk=1,nDispkPerCoord+1)
+
+        enddo
+      
+        close(72)
+
+     enddo
+    endif
+
+    call MPI_BCAST(eigv, size(eigv), MPI_DOUBLE_PRECISION, root, intraPoolComm, ierr)
+    
+    return
+  end subroutine readGroupedEigenvalues
+ 
 end module GVelMod
